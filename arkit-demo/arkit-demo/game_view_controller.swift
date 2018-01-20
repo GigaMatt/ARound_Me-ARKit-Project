fd20cd486382bfd339050cb59bc7927bbbc1a4c0
Matthew Montoya <msmontoya2@miners.utep.edu>
2018-01-20 00:36:31 -0800

    inital xcode configuration

---
+++ arkit-demo/arkit-demo/game_view_controller.swift
@@ -0,0 +1,14 @@
+//
+//  game_view_controller.swift
+//  arkit-demo
+//
+//  Created by Matthew Medina on 1/20/18.
+//  Copyright © 2018 Matthew Medina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SceneKit
+import ARKit
+
+class GameViewController: UIController, ARSCNViewDelegate{
