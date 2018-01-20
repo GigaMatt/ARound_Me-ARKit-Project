0370d444ad3e8c95d450aacd795344854f189a36
cscadmin <ducdao11@gmail.com>
2018-01-20 10:21:44 -0800

    Added back CoordinatesFromFirebase code.

--- arkit-demo/arkit-demo/CoordinatesFromFirebase.swift
+++ arkit-demo/arkit-demo/CoordinatesFromFirebase.swift
@@ -7,7 +7,34 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
 
 class CoordinatesFromFirebase {
+    let getEndpoint: String = "https://sbhacksiv.firebaseio.com/"
     
+    func getLocationCoordinates() {
+        // Run a GET to retrieve JSON data from Firebase
+        Alamofire.request(getEndpoint, method: .get).responseJSON { response in
+            // Check for GET errors
+            guard response.result.error == nil else {
+                // Error getting data
+                print("Error getting coordinates from Firebase!")
+                print(response.result.error!)
+                
+                return
+            }
+            
+            // Check if value recieved is nil
+            guard (response.result.value as? [String:Any]) != nil else {
+                print ("Didn't get JSON response from Firebase!")
+                print ("Error: \(response.result.error!)")
+                
+                return
+            }
+            
+            
+        }
+    }
 }
+
